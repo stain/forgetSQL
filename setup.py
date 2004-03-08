5d098a004bebb21252ed8c56a6c67ed72b8f24e0
Stian Soiland <stian@soiland.no>
2004-03-08 09:33:04 +0100

    Should be ready for distributions now

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+
+# Whyyyyyyy oh whyyy doesn't distutils do this !??
+# (if you forget this - root with umask 0077 
+#  will install non-readable libraries)
+import os
+if os.geteuid() == 0:
+    os.umask(0022)
+
 setup(name="forgetSQL",
-      version="0.4",
+      version="0.5",
       author="Stian Soiland",
       author_email="stian@soiland.no",
       url="http://forgetsql.sourceforge.net/",
@@ -13,5 +21,6 @@ classes that maps SQL tables to objects, normally one class pr. SQL
 table. The idea is to forget everything about SQL and just worrying
 about normal classes and objects.""",
       py_modules=['forgetSQL'],
-      package_dir = {'': 'lib'}
+      scripts = ['bin/forgetsql-generate'],
+      package_dir = {'': 'lib'},
      )
